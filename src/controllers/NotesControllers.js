115e53d32e631d84493cb7d470b4a6d247747c36
Gustavo Santos <devgustavosantos@gmail.com>
2022-09-29 09:19:17 -0300

    refactor: optimizes notes searches by title only

--- src/controllers/NotesControllers.js
+++ src/controllers/NotesControllers.js
@@ -20,7 +20,7 @@ class NotesControllers {
       user_id,
     });
 
-    const tagsOfThisNote = tags.map(tag => {
+    const tagsOfThisNote = tags.map((tag) => {
       const formattedTag = tag.trim();
 
       return {
@@ -69,130 +69,30 @@ class NotesControllers {
   }
 
   async index(request, response) {
-    const { title, rating, tags } = request.query;
+    const { title } = request.query;
     const user_id = request.user.id;
 
     const userInfos = await knex("users").where({ id: user_id }).first();
 
-    dataChecker.userExists(userInfos);
+    const notes = await knex("notes")
+      .where({ user_id })
+      .whereLike("title", `%${title}%`)
+      .orderBy("updated_at");
 
-    let notes;
-    let ratingsInserts;
-    let tagsInserts;
+    const tags = await knex("tags").where({ user_id });
 
-    const queryType = {
-      allTypesOfFilters: tags && rating && title,
+    const notesWithTags = notes.map((note) => {
+      const tagsOfThisNote = tags.filter((tag) => tag.note_id == note.id);
 
-      titleAndRating: title && rating && !tags,
-      titleAndTag: title && tags && !rating,
-      ratingAndTag: rating && tags && !title,
-
-      titleOnly: title && !tags && !rating,
-      ratingOnly: rating && !title && !tags,
-      tagOnly: tags && !title && !rating,
-
-      withoutFilters: !tags && !rating && !title,
-    };
-
-    if (rating) {
-      ratingsInserts = rating.split(",").map(r => r.trim());
-
-      dataChecker.onlyNumbers(ratingsInserts);
-    }
-
-    if (tags) {
-      tagsInserts = tags.split(",").map(tag => tags.trim());
-    }
-
-    if (queryType.allTypesOfFilters) {
-      notes = await knex("tags")
-        .select([
-          "notes.id",
-          "notes.title",
-          "notes.description",
-          "notes.rating",
-        ])
-        .where("notes.user_id", user_id)
-        .whereLike("notes.title", `%${title}%`)
-        .whereIn("notes.rating", ratingsInserts)
-        .whereIn("name", tagsInserts)
-        .innerJoin("notes", "notes.id", "tags.note_id")
-        .orderBy("notes.updated_at");
-    }
-
-    if (queryType.titleAndRating) {
-      notes = await knex("notes")
-        .where({ user_id })
-        .whereLike("title", `%${title}%`)
-        .whereIn("rating", ratingsInserts)
-        .orderBy("updated_at");
-    }
-
-    if (queryType.titleAndTag) {
-      notes = await knex("tags")
-        .select([
-          "notes.id",
-          "notes.title",
-          "notes.description",
-          "notes.rating",
-        ])
-        .where("notes.user_id", user_id)
-        .whereLike("notes.title", `%${title}%`)
-        .whereIn("name", tagsInserts)
-        .innerJoin("notes", "notes.id", "tags.note_id")
-        .orderBy("notes.updated_at");
-    }
-
-    if (queryType.ratingAndTag) {
-      notes = await knex("tags")
-        .select([
-          "notes.id",
-          "notes.title",
-          "notes.description",
-          "notes.rating",
-        ])
-        .where("notes.user_id", user_id)
-        .whereIn("notes.rating", ratingsInserts)
-        .whereIn("name", tagsInserts)
-        .innerJoin("notes", "notes.id", "tags.note_id")
-        .orderBy("notes.updated_at");
-    }
-
-    if (queryType.titleOnly) {
-      notes = await knex("notes")
-        .where({ user_id })
-        .whereLike("title", `%${title}%`)
-        .orderBy("updated_at");
-    }
-
-    if (queryType.ratingOnly) {
-      notes = await knex("notes")
-        .where({ user_id })
-        .whereIn("rating", ratingsInserts)
-        .orderBy("updated_at");
-    }
-
-    if (queryType.tagOnly) {
-      notes = await knex("tags")
-        .select([
-          "notes.id",
-          "notes.title",
-          "notes.description",
-          "notes.user_id",
-        ])
-        .where("notes.user_id", user_id)
-        .whereIn("name", tagsInserts)
-        .innerJoin("notes", "notes.id", "tags.note_id")
-        .orderBy("notes.updated_at");
-    }
-
-    if (queryType.withoutFilters) {
-      notes = await knex("notes").where({ user_id }).orderBy("updated_at");
-    }
+      return {
+        ...note,
+        tags: tagsOfThisNote,
+      };
+    });
 
     dataChecker.didTheSearchGetResults(notes);
 
-    return response.status(201).json(notes);
+    return response.status(201).json(notesWithTags);
   }
 }
 
